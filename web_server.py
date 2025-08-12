ac43ce744a75ebf2dd61d9623429737f1563f057
necrovenom <mohammedshabeerkp2@gmail.com>
2025-08-12 18:18:19 +0000

    minor

--- web_server.py
+++ web_server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sys
 import io
-import requests
+
 from typing import List, Dict, Any
 
 from contextlib import redirect_stdout
